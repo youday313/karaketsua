a290d6ed0489924495a8da25171f3d4c2c23d45c
donuts-sugaya <sugaya.takuma@donuts.ne.jp>
2016-10-22 17:58:38 +0900

    animationCallbacker作成

---
+++ karaketsua/Assets/Scripts/Utility/AnimationCallbacker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Animation))]
+public class AnimationCallbacker : MonoBehaviour {
+
+    public Action OnPlay;
+    public Action Onfinish;
+    public Action[] OnTrigger;
+    private int triggerCount;
+
+    public void Play()
+    {
+        OnPlay();
+    }
+
+    public void Finish()
+    {
+        Onfinish();
+    }
+
+    public void Trigger()
+    {
+        OnTrigger[triggerCount]();
+        triggerCount++;
+    }
+
+    public void ResetTrigger()
+    {
+        triggerCount = 0;
+    }
+}
