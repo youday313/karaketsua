4950bbebcb168696cbce93575480f1bd362a51f1
donuts-sugaya <sugaya.takuma@donuts.ne.jp>
2016-11-06 11:10:10 +0900

    InputBlocker追加

---
+++ karaketsua/Assets/Scripts/Global/InputBlocker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// 画面入力を停止する
+public class InputBlocker : DontDestroySingleton<InputBlocker> {
+
+    [SerializeField] private Button block;
+    // EventSystemやInputTouches
+    [SerializeField] List<GameObject> stopObjects;
+
+    public void Block()
+    {
+        block.interactable = true;
+        stopObjects.ForEach(o => o.SetActive(false));
+    }
+
+    public void Release()
+    {
+        block.interactable = false;
+        stopObjects.ForEach(o => o.SetActive(true));
+    }
+}
