f8358c605a3f1cf007ac5f5f167a17c60f05898b
tetsujp84 <tetsuMac@tetsuMac-no-MacBook-Pro.local>
2016-11-27 09:31:39 +0900

    エフェクト自動削除

---
+++ karaketsua/Assets/Scripts/Utility/DestroyAfterAllParticleSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// 子のパーティクルを含め全てのパーティクル再生後、削除
+public class DestroyAfterAllParticleSystem : MonoBehaviour {
+
+    void Awake()
+    {
+        var particles = GetComponentsInChildren<ParticleSystem>();
+        var maxTime = particles.Max(p => p.duration);
+        Destroy(gameObject, maxTime);
+    }
+}
